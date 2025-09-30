766a0336fcd4922ce9bb49474f31ca9f5baa5a49
Misael <misael.sarda@unidavi.edu.br>
2025-09-29 21:15:20 -0300

    new file:   windows/runner/win32_windo Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const String appTitle = 'Turismo na Suiça';
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(appTitle)),
+        body: const SingleChildScrollView(
+          child: Column(
+            children: [
+              ImageSection(image: 'images/lake.jpg'),
+              TitleSection(
+                name: 'Acampamento do Lago Oeschinen',
+                location: 'Kandersteg, Suiça',
+              ),
+              ButtonSection(),
+              TextSection(
+                description: 'O Lago Oeschinen fica aos pés do Blüemlisalp, nos Alpes Berneses.'
+                ' Situado a 1.578 metros acima do nível do mar, é um dos maiores lagos alpinos.'
+                ' Um passeio de gôndola saindo de Kandersteg, seguido por uma caminhada de meia hora por pastos e florestas de pinheiros,'
+                ' leva você ao lago, que chega a 20 graus Celsius no verão. As atividades disponíveis aqui incluem remo e tobogã no verão.'
+  ),
+            ],// children
+          )
+        ),
+      ),
+    );
+  }
+}
+
+
+class TitleSection extends StatelessWidget {
+  const TitleSection({super.key, required this.name, required this.location});
+
+  final String name;
+  final String location;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*2*/
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    name,
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Text(location, style: TextStyle(color: Colors.grey[500])),
+              ],
+            ),
+          ),
+          /*3*/
+          Icon(Icons.star, color: Colors.red[500]),
+          const Text('41'),
+        ],
+      ),
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final Color color = Theme.of(context).primaryColor;
+    return SizedBox(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          ButtonWithText(color: color, icon: Icons.call, label: 'Ligue'),
+          ButtonWithText(color: color, icon: Icons.near_me, label: 'Rota'),
+          ButtonWithText(color: color, icon: Icons.share, label: 'Compartilhar'),
+        ],
+      ),
+    );
+  }
+
+}
+
+class ButtonWithText extends StatelessWidget {
+  const ButtonWithText({
+    super.key,
+    required this.color,
+    required this.icon,
+    required this.label,
+  });
+
+  final Color color;
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    // A correção está aqui dentro
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Padding(
+          padding: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class TextSection extends StatelessWidget {
+  const TextSection({super.key, required this.description});
+
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Text(description, softWrap: true),
+    );
+  }
+}
+
+class ImageSection extends StatelessWidget {
+  const ImageSection({super.key, required this.image});
+
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(image, width: 600, height: 240, fit: BoxFit.cover);
+  }
+}
